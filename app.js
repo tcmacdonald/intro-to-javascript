7a6271bc9534cc5019ae4574fed02953b4cb63b6
Taylor C. MacDonald <tcmacdonald@gmail.com>
2015-04-15 09:13:54 -0400

    Bind express to Heroku port.

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.get('/', function (req,res) {
 
 app.use("/", express.static(__dirname));
 
-var server = app.listen(3000, function () {
+var server = app.listen(process.env.PORT || 3000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
